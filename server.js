8c83a109c1590d918035d752bd4813e484d5de42
Iftikhar #100Devs <ifti891@gmail.com>
2022-07-30 21:53:08 +0200

    still working on CRUD i am sad that im stuck at these again

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
-// server js
-console.log('may node be with your')
+
+const express = require("express");
+const app = express();
+
+
+app.listen(3000, function(){
+  console.log("listening on 3000")
+})
+
+
+// app.get('/', function(req, res){
+//   res.send("Hello World!");
+// })
+
+// ES6 function of above
+
+// app.get('/', (req, res) => {
+//   res.send("Hello World ES6!");
+// })
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
